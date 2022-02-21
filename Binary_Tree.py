59afa9f9620eb5d63e235b7a968955ab75fb7276
Abhishek Mairpady <abhishekmairpady@gmail.com>
2022-02-21 20:27:28 +0530

    updated

--- Binary_Tree.py
+++ Binary_Tree.py
@@ -71,6 +71,63 @@ class BinarySearchTreeNode:
             else:
                 return False
 
+    def find_min(self):
+        if self.left is None:
+            return self.data
+        if self.left:
+            return self.left.find_min()
+    
+    def find_max(self):
+        if self.right is None:
+            return self.data
+        if self.right:
+            return self.right.find_max()
+        
+    def sum_of_tree(self):
+        sum=self.data
+        if self.left:
+            sum+= self.left.sum_of_tree()
+        if self.right:
+            sum+= self.right.sum_of_tree()
+        return sum
+
+    def delete(self, val):
+        if val < self.data:
+            self.left=self.left.delete(val)
+        elif val> self.data:
+            self.right=self.right.delete(val)
+        else:
+            if self.left is None and self.right is None:
+                return None
+            elif self.left is None:
+                return self.right
+            elif self.right is None:
+                return self.left
+            
+            min_val= self.right.find_min()
+            self.data=min_val
+            self.right.delete(min_val)
+        return self
+
+    def delete1(self,val):
+        if val<self.data:
+            self.left=self.left.delete1(val)
+        elif val>self.data:
+            self.right=self.right.delete1(val)
+        else:
+            if self.left is None and self.right is None:
+                return None
+            elif self.left is None:
+                return self.right
+            elif self.right is None:
+                return self.left
+
+            max_val=self.left.find_max()
+            self.data= max_val
+            self.left.delete1(max_val)
+        return self
+        
+
 def build_tree(elements):
     print("Building tree with these elements:",elements)
     root = BinarySearchTreeNode(elements[0])
@@ -91,3 +148,14 @@ if __name__ == '__main__':
     print("In order traversal gives this sorted list:",numbers_tree.in_order_traversal())
     print(numbers_tree.pre_order_traversal())
     print(numbers_tree.post_order_traversal())
+    print(numbers_tree.find_min())
+    print(numbers_tree.find_max())
+    print(numbers_tree.sum_of_tree())
+
+    numbers_tree1= build_tree([17, 4, 1, 20, 9, 23, 18, 34])
+    numbers_tree1.delete(17)
+    print("After deleting 17 ",numbers_tree1.in_order_traversal())  # this should print [1, 4, 9, 18, 20, 23, 34]
+
+    numbers_tree1= build_tree([17, 4, 1, 20, 9, 23, 18, 34])
+    numbers_tree1.delete1(23)
+    print("After deleting 23 ",numbers_tree1.in_order_traversal())  
